c15fbf9e3c8a4db09995694b2204397267f79da4
Rakib-IIT7 <37789995+Rakib-IIT7@users.noreply.github.com>
2018-03-26 11:25:09 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function(){
+
+
+	jQuery(".middle-area").backstretch([
+		//"images/slider1.jpg",
+		
+		"images/Welcome-Were-Glad-Youre-Here.jpg",
+		"images/Go-vote.png",
+		
+		
+		
+		// "images/slider5.jpg"
+
+		], {
+			duration:5000,
+			transition: 'push_left|push_right|cover_up|cover_down|fade',
+			transitionDuration:1000
+
+		});
+});
